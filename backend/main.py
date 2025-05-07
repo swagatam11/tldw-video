4d41c065dd6cc94d793b33df9f1cb62d98dbc353
Swagatam <swagatam.sinha11@gmail.com>
2025-05-07 16:28:13 +0200

    debugs

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://tldw-video.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
